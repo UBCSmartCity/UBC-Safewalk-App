423a2d4fa1f1dfd9da61816303adea01ec70c8a0
Anshu Shibu <shibuanshu@gmail.com>
2023-01-24 19:17:40 -0800

    Navigation bar changes

--- Frontend/App.js
+++ Frontend/App.js
@@ -3,10 +3,24 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './login.js';
 import HomeScreen from './home.js';
+import { Button, Menu, Divider, Provider,List } from 'react-native-paper';
+import { View } from 'react-native';
+import navScreen from './navScreen.js';
+
 
 const Stack = createNativeStackNavigator();
 
-function App() {
+
+
+//const navigation = useNavigation();
+const MyComponent = () => {
+  
+  const [visible, setVisible] = React.useState(false);
+
+  const openMenu = () => setVisible(true);
+
+  const closeMenu = () => setVisible(false);
+
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
@@ -18,9 +32,13 @@ function App() {
           name="Login" 
           component={LoginScreen} 
         />
+        <Stack.Screen 
+          name="NavScreen" 
+          component={navScreen} 
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
-}
+};
 
-export default App;
+export default MyComponent;
---
+++ Frontend/navScreen.js
@@ -0,0 +1,101 @@
+import {  Image, SafeAreaView, ImageBackground, StyleSheet, Text,  TouchableOpacity, Linking, View , Button} from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { AntDesign } from '@expo/vector-icons';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import HomeScreen from './home';
+import profile from './assets/profile.png'
+
+function navScreen({ navigation }) {
+
+    return (
+      <View style={styles.container}>
+        
+  
+        {/*<Text style = {{ color: '#0047AB', fontSize: 45, fontWeight: 'bold', marginTop: 10}}> SafeWalk </Text> */}
+        
+        <Text style = {{ color: '#FFFFFF', fontSize: 25, fontWeight: 'bold', marginTop:100, marginLeft: 106}}> John Doe </Text>
+        <Text style = {{ color: '#F7F7F7', fontSize: 15, fontWeight: 'normal', marginTop: 5, marginLeft: 109}}> View profile </Text>
+
+        <View style={styles.button2}>
+            <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+                <AntDesign name="closecircleo" size={38} color="white" />
+            </TouchableOpacity>
+        </View>
+
+        <Image source={profile} style={styles.image}></Image>
+
+  
+      {/* presing login button directs you to the home screen */}
+        <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+            <View style={styles.button1}>
+                <AntDesign name="appstore-o" size={30} color="white" />
+            </View> 
+            <Text style = {styles.text1}>Dashboard</Text>
+
+        </TouchableOpacity>
+  
+        <TouchableOpacity onPress={() => navigation.navigate('Login')}> 
+            <View style={styles.button3}>
+                <AntDesign name="setting" size={30} color="white" />
+            </View> 
+            <Text style = {styles.text2}>App Settings</Text>
+ 
+        </TouchableOpacity>
+  
+  
+      </View>
+  
+  
+    );
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#002145'
+    },
+    image: 
+    {
+      flex: 1,
+      resizeMode: 'contain',
+      height: '15%',
+      width:'15%',
+      marginLeft: 15,
+      marginTop:-670
+        
+    },
+
+    text1: 
+    {
+      color: '#FFFFFF', 
+      fontSize: 20, 
+      fontWeight: 'bold', 
+      marginTop: -30,
+      marginLeft: 90
+    },
+    text2: 
+    {
+      color: '#FFFFFF', 
+      fontSize: 20, 
+      fontWeight: 'bold', 
+      top: -448,
+      marginLeft: 90
+    },
+    button1:
+    {
+        marginTop:-505,
+        marginLeft:30  
+    },
+    button2:
+    {
+      marginTop:-45,
+      marginLeft:360 
+    },
+    button3:
+    {
+      bottom:420,
+      marginLeft:30 
+    }
+  });
+  
+  export default navScreen;
