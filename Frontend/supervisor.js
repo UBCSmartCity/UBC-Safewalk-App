033d19733c5392ca40999f11345be342e32b21f4
Jerrysun817 <sunyiran0817@gmail.com>
2023-01-25 15:51:11 -0800

    added switch

--- Frontend/supervisor.js
+++ Frontend/supervisor.js
@@ -1,15 +1,17 @@
-import * as React from 'react';
-import { Text, View, StyleSheet , SafeAreaView, VirtualizedList } from 'react-native';
+// import * as React from 'react';
+import React, {useState} from 'react';
+import { Text, View, StyleSheet , SafeAreaView, VirtualizedList, Switch} from 'react-native';
 import Constants from 'expo-constants';
 
 
 // Need to add dependency 
+// use "npm install react-native-dropdown-select-list"
 import { SelectList } from 'react-native-dropdown-select-list';
 
 // or any pure javascript modules available in npm
 import { Card } from 'react-native-paper';
 
-const getItem = (_data, index) => ({
+  const getItem = (_data, index) => ({
     id: Math.random().toString(12).substring(0),
     title: `Person ${index + 1}`,
   });
@@ -31,6 +33,10 @@ export default function Supervisor() {
       {key:'3', value:'Next Week'},
       {key:'4', value:'All Time'},
   ]
+
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(previousState => !previousState);
+
   return (
     <View style={styles.container}>
       
@@ -44,6 +50,22 @@ export default function Supervisor() {
       />
       
     <SafeAreaView style={styles.container}>
+    
+    <View style={{ flexDirection: "row"}}>
+      <Text style={{}}>
+          Unassigned
+      </Text>
+      <Switch
+        trackColor={{false: '#767577', true: '#81b0ff'}}
+        thumbColor={isEnabled ? '#f5dd4b' : '#f4f3f4'}
+        ios_backgroundColor="#3e3e3e"
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+      />
+      <Text style={{}}>
+        Assigned
+      </Text>
+    </View>
     
     <VirtualizedList
         initialNumToRender={4}
@@ -62,6 +84,11 @@ export default function Supervisor() {
 }
 
 const styles = StyleSheet.create({
+  containerSW: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
