4cace31ee2be0b10d17c29498f63aa059e77b30e
Anshu Shibu <shibuanshu@gmail.com>
2023-01-27 18:52:22 -0800

    menu navigation update

--- Frontend/App.js
+++ Frontend/App.js
@@ -6,40 +6,63 @@ import HomeScreen from './home.js';
 import { Button, Menu, Divider, Provider,List } from 'react-native-paper';
 import { View } from 'react-native';
 import navScreen from './navScreen.js';
+import { TouchableOpacity, Text } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
 
+import { useState } from 'react';
+import { Image, StyleSheet } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
+const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
 
-const Stack = createNativeStackNavigator();
-
-
-
-//const navigation = useNavigation();
-const MyComponent = () => {
-  
-  const [visible, setVisible] = React.useState(false);
-
-  const openMenu = () => setVisible(true);
-
-  const closeMenu = () => setVisible(false);
-
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen 
-          name="Home" 
-          component={HomeScreen} 
-        />
-        <Stack.Screen 
-          name="Login" 
-          component={LoginScreen} 
-        />
-        <Stack.Screen 
-          name="NavScreen" 
-          component={navScreen} 
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+const MenuScreen = ({ navigation }) => {
+return (
+<View style={styles.container}>
+<Text>Home Screen</Text>
+<TouchableOpacity onPress={() => navigation.openDrawer()}>
+<Image source={require('./assets/hamburger-icon.png')} style={styles.hamburgerIcon} />
+</TouchableOpacity>
+</View>
+);
 };
 
-export default MyComponent;
+
+
+const DrawerNavigator = () => {
+return (
+<Drawer.Navigator useLegacyImplementation={true}>
+<Drawer.Screen name="Home" component={HomeScreen} />
+<Drawer.Screen name="Login" component={LoginScreen} />
+<Drawer.Screen name="Settings" component={navScreen} />
+</Drawer.Navigator>
+);
+};
+
+const App = () => {
+return (
+  <NavigationContainer>
+    <Stack.Navigator>
+    <Stack.Screen name="DrawerNavigator" component={DrawerNavigator} options={{ headerShown: false }} />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
+};
+
+const styles = StyleSheet.create({
+container: {
+flex: 1,
+alignItems: 'center',
+justifyContent: 'center',
+},
+hamburgerIcon: {
+width: 30,
+height: 30,
+margin: 10,
+},
+});
+
+export default App;
+
